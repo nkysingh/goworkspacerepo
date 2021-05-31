952faebc45a2ea7da2e3cf3ecbcc0398111a9829
Vijendra Chandel <chandel_vi@India.XoriantCorp.com>
2021-05-31 10:40:42 +0530

    hello prpo changes

--- hello.go
+++ hello.go
@@ -1,6 +1,20 @@
 package main
 import "fmt"
-
-func main(){
+func main() {
 	fmt.Println("hello")
+
+	foo()
+
+	n,_ := fmt.Println("somehting,434, random words")
+	fmt.Println(n)
+	for i:= 0; i < 20; i++{
+		if i%2 == 0{
+			fmt.Println(i)
+		}
+	}
 }
+
+func foo() {
+	fmt.Println("im foo")
+}
+
